bd7884dd14fad840714c52806ec305c6cf943919
Gtd232 <im.gtd232@outlook.com>
2024-02-01 13:21:50 +0800

    压缩

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # status-axolotlpower-com-cfpages
-status.axolotlpower.com的Cloudflare Pages部分
-
-本地调试:
-1.安装`wrangler`: `npm i -g @cloudflare/wrangler`
-2.运行:`wrangler pages dev ./`
+status.axolotlpower.com的Cloudflare Pages部分  
+Production环境,已压缩.
--- functions/get.js
+++ functions/get.js
@@ -1,6 +1,1 @@
-//By Gtd232
-
-export async function onRequest(context) {
-	//context.env.STATUS_INFO.get('DATA')
-	return new Response(await context.env.STATUS_INFO.get('DATA'), {headers : { 'Content-Type': 'application/json' }});
-};
+export async function onRequest(context){return new Response(await context.env.STATUS_INFO.get('DATA'),{headers:{'Content-Type':'application/json'}});};
